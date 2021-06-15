8a3ec9c43e39ad16882f63b45b3a72fbc7bbf2c8
RushikeshBhandare <rushi5049@gmail.com>
2021-06-16 03:51:16 +0530

    changes in the packege file and added a cors package

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const nodemailer = require('nodemailer')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 
